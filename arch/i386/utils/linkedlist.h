3bd0e5e9bbf55271484e108ea332ad9670bf7ce1
Maciej Piechotka <uzytkownik2@gmail.com>
2009-01-01 00:10:59 +0100

    Added queue support

--- arch/i386/utils/linkedlist.h
+++ arch/i386/utils/linkedlist.h
@@ -21,17 +21,81 @@
 			"\tlock cmpxchg %%ecx, %0\n"			\
 			"\tjnz 1b\n"					\
 			"1:"						\
-			: "=m"(stack.head), "=a" (node)			\
-			: "m"(stack.head), "a" (stack.head)		\
-			: "%ecx" );
+			: "=m"((stack).head), "=a"(node)		\
+			: "m"((stack).head), "a"((stack).head)		\
+			: "%ecx");
 #define STACK_PUSH(stack, node)						\
  __asm__ __volatile__ ("\n"						\
 		       "1:\n"						\
 		       "\tmov %%eax, (%%ebx)\n"				\
 		       "\tlock cmpxchg %%ebx, %0\n"			\
 		       "\tjnz 1b\t"					\
-		       : "=m"(stack.head), "=m"(node->next)		\
-		       : "m" (stack.head), "b"(node),			\
-		       "a" (stack.head));				\
+		       : "=m"((stack).head), "=m"((node)->next)		\
+		       : "m" ((stack).head), "b"((node)),		\
+		       "a" ((stack).head));				\
 
+#define QUEUE(name, data)					\
+  struct name ## _node						\
+  {								\
+    struct name ## _node *next;					\
+    data;							\
+  };								\
+								\
+  struct name							\
+  {								\
+    volatile struct name ## _node *head;			\
+    volatile struct name ## _node *tail;			\
+  };
+
+#define QUEUE_PUSH(queue, node)						\
+  __asm__ __volatile__ ("\n"						\
+			"1:\n"						\
+			"\tmov %0, %%ebx\n"				\
+			"\tmov (%%ebx), %%ecx\n"			\
+			"\tcmp %%ebx, %0\n"				\
+			"\tjnz 1b\n"					\
+			"\ttest %%ecx, %%ecx\n"				\
+			"\tjnz 2f\n"					\
+			"\txor %%eax, %%eax\n"				\
+			"\tlock cmpxchg %%edx, (%%ecx)\n"		\
+			"\tjnz 1b\n"					\
+			"\tjmp 3f\n"					\
+			"2:\n"						\
+			"\tmov %%ebx, %%eax\n"				\
+			"\tlock cmpxchg %%ecx, %0\n"			\
+			"\tjmp 1b\n"					\
+			"3:\n"						\
+			: "=m"((queue).tail)				\
+			: "m"((queue).tail), "d"((node))		\
+			: "%eax", "%ebx", "%ecx")
+
+#define QUEUE_POP(queue, head, old_head) \
+  __asm__ __volatile__ ("\n"						\
+			"1:\n"						\
+			"\tmov %0, %%ebx\n"				\
+			"\tmov %1, %%ecx\n"				\
+			"\tmov (%%ebx), %%edx\n"			\
+			"\tcmp %0, %%ebx\n"				\
+			"\tjnz 1b\n"					\
+			"\tcmp %%ebx, %%ecx"				\
+			"\tjnz 2f\n"					\
+			"\ttest %%edx, %%edx\n"				\
+			"\tjnz 3f\n"					\
+			"\txor %%eax, %%eax\n"				\
+			"\txor %%edx, %%edx\n"				\
+			"\tjmp 4f\n"					\
+			"3:"						\
+			"\tmov %%ecx, %%eax\n"				\
+			"\tlock cmpxchg %%edx, %1\n"			\
+			"\tjmp 1b\n"					\
+			"2:"						\
+			"\tmov %%ebx, %%eax\n"				\
+			"\tlock cmpxchg %%edx, %0\n"			\
+			"\tjnz 1b\n"					\
+			"4:"						\
+  			: "=m"((queue).head), "=m"((queue).tail)	\
+			  "=a"(old_head), "=d"(head)			\
+			: "m"((queue).head), "m"((queue).tail)		\
+			: )
+    
 #endif
